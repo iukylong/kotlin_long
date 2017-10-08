e00a185a8aa965ea139ebd94c976df4bfdb2398e
root <root@localhost.localdomain>
2017-10-08 14:16:02 -0400

    test

--- app/src/main/java/longtv9/fsoft/root/lesson1/MainActivity.kt
+++ app/src/main/java/longtv9/fsoft/root/lesson1/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d(TAG,"OnCreate")
+        Log.d(TAG,"OnCreate duoc tao")
     }
 
     override fun onStart() {
